9a285a60386b621ae238ac1473c7f68cf143e38e
Angela Mitchell <anglarenmit@gmail.com>
2021-01-04 16:45:56 -0600

    Created projectForm with controls, default value for status, and synchronized with template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  statusOptions: string[] = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      status: new FormControl('Stable'),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
 }
