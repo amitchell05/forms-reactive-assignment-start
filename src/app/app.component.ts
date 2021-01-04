7427fc2a18b2643625fa03517a2b5689eb079c23
Angela Mitchell <anglarenmit@gmail.com>
2021-01-04 17:03:52 -0600

    Created custom validator for project name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,28 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   projectForm: FormGroup;
   statusOptions: string[] = ['Stable', 'Critical', 'Finished'];
+  forbiddenProjectNames: string[] = ['Test'];
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      projectName: new FormControl(null, Validators.required),
+      projectName: new FormControl(null, [
+        Validators.required,
+        this.forbiddenNames.bind(this),
+      ]),
       email: new FormControl(null, [Validators.required, Validators.email]),
       status: new FormControl('Stable'),
     });
   }
 
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    } else {
+      return null;
+    }
+  }
+
   onSubmit() {
-    console.log(this.projectForm);
+    console.log(this.projectForm.value);
   }
 }
