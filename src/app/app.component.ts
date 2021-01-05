69953be2e91de98d09580dab396578590dbb6191
Angela Mitchell <anglarenmit@gmail.com>
2021-01-05 00:24:16 -0600

    Part 1 of Max's alternative solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
@@ -9,46 +9,19 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   projectForm: FormGroup;
-  statusOptions: string[] = ['Stable', 'Critical', 'Finished'];
-  forbiddenProjectNames: string[] = ['Test'];
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      projectName: new FormControl(
-        null,
-        [Validators.required],
-        this.forbiddenNames
-      ),
+      projectName: new FormControl(null, [
+        Validators.required,
+        CustomValidators.invalidProjectName,
+      ]),
       email: new FormControl(null, [Validators.required, Validators.email]),
-      projectStatus: new FormControl('Critical'),
+      projectStatus: new FormControl('critical'),
     });
-  }
-
-  // Non-async validator
-
-  // forbiddenNames(control: FormControl): { [s: string]: boolean } {
-  //   if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
-  //     return { nameIsForbidden: true };
-  //   } else {
-  //     return null;
-  //   }
-  // }
-
-  forbiddenNames(control: FormControl): Promise<any> | Observable<any> {
-    const promise = new Promise<any>((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value === 'Test') {
-          resolve({ nameIsForbidden: true });
-        } else {
-          resolve(null);
-        }
-      }, 1500);
-    });
-    return promise;
   }
 
   onSaveProject() {
     console.log(this.projectForm.value);
-    this.projectForm.reset();
   }
 }
